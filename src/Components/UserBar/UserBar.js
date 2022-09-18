f07cfdaa1aba98a9f37d0e3e9812ba2d0e1be68b
Daniel Fedorca <fedorcadaniel116@gmail.com>
2022-09-18 11:03:38 +0300

    default Props for user

--- src/Components/UserBar/UserBar.js
+++ src/Components/UserBar/UserBar.js
@@ -1,6 +1,6 @@
 import './UserBar.css'
 
-const UserBar = () => {
+const UserBar = (props) => {
     return (
         <div className = "user-bar-container">
             <div className = "user-image">
@@ -13,6 +13,17 @@ const UserBar = () => {
 
         </div>
     );
+}
+
+UserBar.defaultProps = {
+    name: "nume/adresa", //ENS or adress
+    skill: 0, // average score
+    sillsNumber: 0, // the number of aquired skills
+    givenEndorsements : 0, //the number of givenEndorsements
+    takenEndorsements: 0, //the number of taken endorsements
+    imageURL: "#", // the adress for the user image
+    generalDescription: "Nothing" // a general description for put by user
+
 }
 
 export default UserBar
